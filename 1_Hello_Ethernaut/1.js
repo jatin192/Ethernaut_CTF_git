280e1dda5ce923f03b1aed0663e02f4ab4a952dc
jatin192 <singhaljatin192@gmail.com>
2024-02-01 16:32:40 +0530

    provider signer

---
+++ 1_Hello_Ethernaut/1.js
@@ -0,0 +1,25 @@
+let { ethers } = require('ethers');
+
+let ABI = require('./ABI.json');
+let PRIVATE_KEY = "";
+let network = "";
+let Contract_address = "0xfB8Dd40b453ed945cB60E1C6655f16E045a64bf6";
+
+
+let provider = new ethers.providers.JsonRpcProvider(network);      // read from blockchain
+let signer = new ethers.Wallet(PRIVATE_KEY, provider);            //  write on blockchain
+
+let i = new ethers.Contract(Contract_address,ABI,signer);       // contract_instance
+
+let a = async()=>
+{
+    let password_ = await i.password();
+    console.log("password =",password_);
+    await i.authenticate(password_);
+    console.log("contract address =",i.address);
+}
+a().catch((error) => 
+{
+    console.log("Error aagya ===")
+    console.error(error);
+});
