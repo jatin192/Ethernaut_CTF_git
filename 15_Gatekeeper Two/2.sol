53a2eeaa3f2594ae3a65d73228114948465d18a7
jatin192 <singhaljatin192@gmail.com>
2024-02-16 03:17:37 +0530

    address(adr).code  /  Assembly opcode extcodesize(adr)  / .code.length

--- 15_Gatekeeper Two/2.sol
+++ 15_Gatekeeper Two/2.sol
@@ -10,15 +10,31 @@ contract A
 
     2.  opcode extcodesize(adr)	    ->     size of the code at address adr
                                     ->     only returns the correct value once the contract is constructed. 
-                                    ->     Returns 0 if it is called from the constructor of a contract.                                  */
+                                    ->     Returns 0 if it is called from the constructor of a contract.   
+    
+    3.  modifier onlyEoa() 
+        {
+            require(tx.origin == msg.sender, "Not EOA");
+            _;
+        }                                                                                                                                                     */
     
 
+    //1
+    modifier onlyEoa() 
+    {
+        require(tx.origin == msg.sender, "Not EOA");
+        _;
+    }   
     
+
+    //2
     function isContract1(address adr) public view returns (bool)
     {
         return (adr.code.length > 0);
     }
 
+
+    //3
     function isContract2(address _addr) public view returns (bool)
     {
         uint32 size;
@@ -29,5 +45,6 @@ contract A
         return (size > 0);
     }
 
+    //understanding
     function getCode(address a) public view returns (bytes memory) { return a.code; } 
 }
